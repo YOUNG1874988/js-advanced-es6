6f19cfebb0d9cb1960a1bf22784b0267c0b39d63
YOUNG1874988 <YOUNG1874988>
2020-01-10 15:51:01 +0800

    initial commit

---
+++ tab.js
@@ -0,0 +1,28 @@
+//创建类
+class Tab {
+
+    constructor(id) {
+        this.main = document.querySelector(id)
+    }
+
+//    1.    切换功能
+    toggleTab() {
+    }
+
+//    2.添加功能
+    addTab() {
+    }
+
+//    3.删除功能
+    removeTab() {
+
+    }
+
+    // 4.修改功能
+    editTab() {
+
+    }
+}
+
+//创建类实例
+new Tab();
